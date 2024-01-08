fa71a0ba88ee7d0d85310a97b27c49cd662ee36f
AndrewBennettDev <andrewbennettdev@gmail.com>
2024-01-08 09:49:35 -0500

    initial commit, basic structure and boilerplate code

---
+++ py_weather_app/main.py
@@ -0,0 +1,24 @@
+import http.client
+
+
+def get_current_weather(location: int) -> str:
+  conn = http.client.HTTPSConnection("weatherapi-com.p.rapidapi.com")
+
+  headers = {
+      'X-RapidAPI-Key': "c9918a8ceamsh71bb6a68f0c1a2cp1fbbcbjsnda7ba6738dea",
+      'X-RapidAPI-Host': "weatherapi-com.p.rapidapi.com"
+  }
+
+  conn.request("GET", f"/current.json?q={location}", headers=headers)
+
+  res = conn.getresponse()
+  data = res.read()
+
+  print(data.decode("utf-8"))
+
+def main():
+  location = input("Enter Zip Code: ")
+  get_current_weather(location)
+
+if __name__ == "__main__":
+  main()
